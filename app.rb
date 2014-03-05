594bb1130d2d20283fee06cebd9fbbc3ce1a13bc
Sosena Lemma <solemma@gmail.com>
2014-03-04 21:23:52 -0500

    Third Commit

--- app.rb
+++ app.rb
@@ -9,12 +9,16 @@ set :database, "sqlite3:///new_project.sqlite3"
 
 set :sessions, true
 
+def current_user
+	if session[:user_id]
+		User.find(session[:user_id])
+	else
+		nil 
+	end
+end
+
 require './models'
 
-# get'/sup' do 
-# 	puts "THESE ARE MY PARAMS"
-# 	puts params.inspect
-# end
 
 get '/' do
 	# User.create(fname: "Sos")
@@ -23,21 +27,53 @@ get '/' do
 end
 
 get '/profile' do
+	@user=current_user
 	erb :profile
 end
 
+get '/sign_up' do
+	erb :sign_up
+end
 
+post '/sign-up' do
+	@newuser=User.create(params[:user])
+	redirect '/create-profile'
+end
 
+get '/create-profile' do
+	erb :create_profile
+end
+
+post '/create-profile' do
+	@newprofile=Profile.create(params[:profile])
+	redirect '/'
+end
+
+get '/create-post' do
+	erb :create_post
+end
+
+post '/create-post' do
+	@newpost=Post.new(params[:post])
+	@newpost.user_id = current_user.id
+	@newpost.save
+	redirect '/profile'
+end
+
+get '/sign-out' do
+	session[:user_id]=nil
+end
 
 post '/sign-in' do
 	@user=User.where(username: params[:username]).first
 	if @user && @user.password==params[:password]
 		flash[:notice]="You've Successfully Signed In"
+		session[:user_id] = @user.id
 		redirect "/profile"
 	else
-		flash[:notice]="Username/Password Combination Incorrect"
-		redirect "/"
-		erb :sign_in
+		flash[:notice]="Invalid username/Password combination, create a new account"
+		redirect "/sign_up"
+		erb :sign_up
 		erb :layout
 	end
 end
