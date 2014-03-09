e2bd856d2a6698bb8a81a53ecb5cc495c02ee696
Sosena Lemma <solemma@gmail.com>
2014-03-09 11:56:19 -0400

    9th commit

--- Gemfile
+++ Gemfile
@@ -3,7 +3,6 @@ ruby '1.9.3'
 
 gem 'activerecord'
 gem 'sinatra-activerecord'
-gem 'sqlite3'
 gem 'rake'
 gem 'rack-flash3', :git=> "git://github.com/treeder/rack-flash.git"
 
--- app.rb
+++ app.rb
@@ -5,25 +5,42 @@ require 'rack-flash'
 enable :sessions
 use Rack::Flash, :sweep=>true
 
-configure (:development) {set :database, "sqlite://blog.sqlite3"}
+configure (:development) {set :database, "sqlite:///blog.sqlite3"}
+set :sessions, true
 
 set :database, "sqlite3:///new_project.sqlite3"
 
-set :sessions, true
+require './models'
 
 def current_user
 	if session[:user_id]
 		User.find(session[:user_id])
 	else
-		nil 
+		nil
 	end
 end
 
-require './models'
-
 
 get '/' do
+	@user=current_user
 	erb :home
+end
+
+post '/sessions/new' do
+end
+
+post '/sign-in' do
+	@user=User.where(username: params[:username]).first
+	if @user && @user.password==params[:password]
+		flash[:notice]="You've Successfully Signed In!"
+		session[:user_id] = @user.id
+		redirect "/profile"
+	else
+		flash[:notice]="Invalid username/Password combination, create a new account"
+		redirect "/sign-up"
+		erb :sign_up
+		erb :layout
+	end
 end
 
 get '/profile' do
@@ -31,24 +48,26 @@ get '/profile' do
 	erb :profile
 end
 
-get '/sign_up' do
+get '/sign-up' do
 	erb :sign_up
 end
 
 post '/sign-up' do
 	@newuser=User.create(params[:user])
+	session[:user_id] = @newuser.id
 	redirect '/create-profile'
 end
 
 get '/create-profile' do
 	erb :create_profile
+	@newuser=current_user
 end
 
 post '/create-profile' do
 	@newprofile=Profile.new(params[:profile])
-	@newprofile.user_id = current_user.id
+	@newprofile.user_id = session [:user_id]
 	@newprofile.save
-	redirect '/profile'
+	redirect '/'
 end
 
 get '/create-post' do
@@ -66,17 +85,7 @@ get '/sign-out' do
 	session[:user_id]=nil
 end
 
-post '/sign-in' do
-	@user=User.where(username: params[:username]).first
-	if @user && @user.password==params[:password]
-		flash[:notice]="You've Successfully Signed In!"
-		session[:user_id] = @user.id
-		redirect "/profile"
-	else
-		flash[:notice]="Invalid username/Password combination, create a new account"
-		redirect "/sign_up"
-		erb :sign_up
-		erb :layout
-	end
+get '/logout' do
+	session[:user_id] = nil
+	redirect '/'
 end
-
